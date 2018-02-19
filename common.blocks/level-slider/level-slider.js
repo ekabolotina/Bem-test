278eb4a88175cfc16d37302d27a756cdbba05c17
Иван <ivan.rusia@mail.ru>
2018-02-19 17:47:53 +1000

    Сделать глобальные перменные блока .level-slider локальными

--- common.blocks/level-slider/level-slider.js
+++ common.blocks/level-slider/level-slider.js
@@ -3,14 +3,12 @@ var $levelSliders = document.querySelectorAll('.level-slider');
 for(var i = 0; i < $levelSliders.length; i++){
     $levelSlider = $levelSliders[i],
     $levelSlider__handle = $levelSlider.querySelector('.level-slider__handle'),
-    dragging = false,
-    levelSlider_posX = $levelSlider.getBoundingClientRect().left,
-    levelSlider_width = $levelSlider.clientWidth;
+    dragging = false;
 
     $levelSlider.onmousedown = function(e){
         e.preventDefault();
         dragging = true;
-        __levelSlider_moveTo(e.clientX - levelSlider_posX);
+        __levelSlider_moveTo(e.clientX - $levelSlider.getBoundingClientRect().left);
     }
 
     $levelSlider.onmouseup = function(e){
@@ -20,7 +18,7 @@ for(var i = 0; i < $levelSliders.length; i++){
 
     document.onmousemove = function(e){
         if(!dragging) return;
-        __levelSlider_moveTo(e.clientX - levelSlider_posX);
+        __levelSlider_moveTo(e.clientX - $levelSlider.getBoundingClientRect().left);
     }
 
     document.onmouseup = function(e){
@@ -30,6 +28,7 @@ for(var i = 0; i < $levelSliders.length; i++){
 }
 
 function __levelSlider_moveTo(pos){
+    var levelSlider_width = $levelSlider.clientWidth;
     if(pos < 0){
         $levelSlider__handle.style.left = 0;
         return;
